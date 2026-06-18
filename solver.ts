d179b47444741e4cb5e2cdd6c1d45848b9557cd7
akumafahuda <akumafahuda@gmail.com>
2026-06-18 16:50:18 +0700

    Update solver.ts

--- solver.ts
+++ solver.ts
@@ -5,7 +5,7 @@ const B_TIMEOUT = 120_000
 const P_TIMEOUT = 300_000
 const NAV_TIMEOUT = 30_000
 const MIN_LEN = 10
-const MAX_CONCURRENT_BROWSERS = 3
+const MAX_CONCURRENT_BROWSERS = 12
 
 function cleanHtml(str: string): string {
   return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#039;')
